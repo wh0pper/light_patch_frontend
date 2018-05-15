a0c7615ec74d38ca29e53d79eb300da6383fdac0
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-05-15 12:11:01 -0700

    add global on/off switch

--- src/app/custom-material.module.ts
+++ src/app/custom-material.module.ts
@@ -10,7 +10,9 @@ import {
   MatSelectModule,
   MatOptionModule,
   MatRadioModule,
-  MatSliderModule
+  MatSliderModule,
+  MatTabsModule,
+  MatSlideToggleModule
 } from '@angular/material';
 
 @NgModule({
@@ -24,7 +26,9 @@ import {
     MatSelectModule,
     MatOptionModule,
     MatRadioModule,
-    MatSliderModule
+    MatSliderModule,
+    MatTabsModule,
+    MatSlideToggleModule
   ],
   exports: [
     MatButtonModule,
@@ -36,7 +40,9 @@ import {
     MatSelectModule,
     MatOptionModule,
     MatRadioModule,
-    MatSliderModule
+    MatSliderModule,
+    MatTabsModule,
+    MatSlideToggleModule
   ]
 })
 
