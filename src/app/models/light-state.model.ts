3d30ffbecc9e9a665ecc7d7b1c98b4ebcf8933b9
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-05-21 20:49:41 -0700

    fix state model for build error

--- src/app/models/light-state.model.ts
+++ src/app/models/light-state.model.ts
@@ -1,14 +1,8 @@
 export interace LightState {
-
-  // constructor(
     active: boolean;
     name: string;
     mode: string;
     brightness: number;
     color: string;
     id: number;
-    // public schedule?: string,
-    // public timer?: number
-  // ) { }
-
 }
